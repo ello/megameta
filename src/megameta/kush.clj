6ae4062d365c8d62d1314612852db4d03d31d9f8
Micah Adams <micah@ello.co>
2015-06-06 21:56:54 -0600

    Better naming in HTML puller
    Needed to differentiate between our HTML puller (which uses enlive) and
    something we may use for our OEmbed puller.

--- src/megameta/kush.clj
+++ src/megameta/kush.clj
@@ -2,12 +2,9 @@
   (:use [clojure.string :refer [blank?]])
   (:require [net.cgrand.enlive-html :as html]))
 
-(defn pull-resource [url]
+(defn- pull-html-resource [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn og-match [str]
-  (not (blank? str)))
-
-(defn get-metas [url]
-  (let [metas (filter :property (map :attrs (html/select (pull-resource url) [:html :head :meta])))]
+(defn get-og-tags [url]
+  (let [metas (filter :property (map :attrs (html/select (pull-html-resource url) [:html :head :meta])))]
     (filter #(re-find #"og:(.*)" (:property %)) metas)))
